e877155da67ea68a4a13118c4805bae601f4faca
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-16 21:26:27 -0800

    added layout changes, adding provider for changing theme, button for clicking to projects. hero.tsx added for hero area with squares and textgeenerateeffect  and added textgenerateeffect for the text

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,5 +1,7 @@
+import BorderMagicButton from "./BorderMagicButton";
 import { Spotlight } from "./Spotlight";
-
+import { TextGenerateEffect } from "./TextGenerateEffect";
+import { GoArrowDownRight } from "react-icons/go";
 
 const Hero = () => {
   return (
@@ -19,9 +21,26 @@ const Hero = () => {
             fill="blue"
           />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.06] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        </div>
+
+        <div className="flex justify-center relative my-20 z-10">
+            <div className="mx-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center">
+                <h2 className="uppercase tracking-widest text-xs text-center text-blue-400 max-w-80">Dynamic web Magic Next.js</h2>
+
+                <TextGenerateEffect 
+                    className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                    words={"Hey there! I'm your friendly neighborhood junior software engineering"}
+                />
+                {/* have the text below fad in after the textgenerate effect component above */}
+                <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white">Hi, I&apos;m David a Next.js Developer based in Stockton,CA</p>
+
+                {/* add the button below here */}
+                <a href="#about"><BorderMagicButton  title="My Projects" icon={<GoArrowDownRight />} position="right"/></a>
+                
+            </div>
         </div>
 
       </div>
