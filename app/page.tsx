c98141f178604296274f5238c097592e7afd929a
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-17 19:52:59 -0800

    added layout changes, grids, bentogrid, navbar as well and index data file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/ui/Hero";
-
+import { FaHome } from "react-icons/fa";
 
 
 
@@ -8,7 +10,9 @@ export default function Home() {
    <main className="relative h-full bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
     <div className="max-w-7xl w-full ">
       <h1 className="text-white-200">Hello, Portfolio</h1>
+      <FloatingNav navItems={[{name:"Home",link:'/',icon:<FaHome />}]}/>
       <Hero/>
+      <Grid/>
     </div>
    </main>
   );
---
+++ components/Grid.tsx
@@ -0,0 +1,31 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <section id="#about">
+      <BentoGrid>
+        {gridItems.map(
+          ({ title, description, id, className, img, imgClassName, titleClassName, spareImg }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
+}
+
+export default Grid
+
