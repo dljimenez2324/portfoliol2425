a15c3f80b4c0ccd29944813f0f8f214fa1102079
dljimenez2324 <dljimenez2324@gmail.com>
2024-12-19 19:06:51 -0800

    added 3dpin, recentproejcts file and pin container

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/ui/Hero";
 import { FaHome } from "react-icons/fa";
@@ -13,6 +14,7 @@ export default function Home() {
       <FloatingNav navItems={[{name:"Home",link:'/',icon:<FaHome />}]}/>
       <Hero/>
       <Grid/>
+      <RecentProjects/>
     </div>
    </main>
   );
